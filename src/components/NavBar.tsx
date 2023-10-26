83b254cae31be2b4ecfd8536cee59cb233b62040
Dounya <dounya.berramdane@edu.esiee-it.fr>
2023-10-26 14:53:56 +0200

    Calendar finish

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -6,7 +6,7 @@ const strokeWidth = 1.5;
 
 function NavBar() {
   return (
-    <div className="flex justify-between sticky bg-[rgba(65,65,65,0.77)]   p-8  ">
+    <div className="flex justify-between sticky bg-[rgba(65,65,65,0.77)] p-8">
       <ul className="flex flex-row max-w-xl gap-5">
         <li>
           <a href="#">Home</a>
@@ -21,23 +21,23 @@ function NavBar() {
           <a href="#">Favoris</a>
         </li>
       </ul>
-      <div className="flex flex-row gap-3">
+      <div className="flex items-center w-3/5 gap-3">
         <input
-          className=" px-4 py-1 w-full rounded-md bg-[#242424] focus:outline-none focus:ring-2 focus:ring-[#6643b5] focus:border-transparent"
+          className="flex-grow px-4 py-1 rounded-md bg-[#242424] focus:outline-none focus:ring-2 focus:ring-[#6643b5] focus:border-transparent"
           placeholder="Search"
         />
         <Search size={28} color="#ffffff" strokeWidth={1.5} />
       </div>
       <div className="flex flex-row gap-5">
         <BellDot
-          className="cursor-pointer	"
+          className="cursor-pointer"
           size={28}
           color="#ffffff"
           strokeWidth={strokeWidth}
           onClick={() => console.log("testBell")}
         />
         <UserCircle2
-          className="cursor-pointer	"
+          className="cursor-pointer"
           size={28}
           color="#ffffff"
           strokeWidth={strokeWidth}
--- src/pages/calendar/Day.tsx
+++ src/pages/calendar/Day.tsx
@@ -1,10 +1,29 @@
 interface DayProps {
   day: string;
+  seriesList: any[];
 }
-export default function Day({ day }: DayProps) {
+
+export default function Day({ day, seriesList }: DayProps) {
   return (
-    <div className="flex flex-col  w-3/5 mt-[25px] ml-2.5px bg-[rgba(65,65,65,0.77)] border border-[#6643b5] p-8 rounded-lg">
-      {day}
+    <div className="bg-[rgba(65,65,65,0.77)] border border-[#6643b5] p-3 rounded-lg flex flex-col">
+      <h2 className="mb-3 text-xl font-bold">{day}</h2>
+      <div className="flex flex-wrap -m-2">
+        {seriesList.map((series) => (
+          <div
+            className="flex m-2 justify-center align-middle "
+            key={series.title}
+          >
+            <img
+              src={series.image_url}
+              alt={series.title}
+              className="h-20 w-auto object-cover mr-2 rounded-lg"
+            />
+            <div className="flex flex-col">
+              <div className="text-sm">{series.title}</div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
