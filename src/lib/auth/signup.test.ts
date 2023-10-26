2b7d030d043ac7eb9588a0fbc522f365db1a54b2
justoverfr <jeanjuste.lefebvre@gmail.com>
2023-10-26 09:28:37 +0200

    Update test files

--- src/lib/auth/signup.test.ts
+++ src/lib/auth/signup.test.ts
@@ -1,13 +1,9 @@
 import { expect, test } from "vitest";
 import { signup } from "./signup";
-import { login } from "./login";
-import { logout } from "./logout";
 
 const email = "johndoe@gmail.com";
 const password = "password";
 
-test("auth", async () => {
+test("signup", async () => {
   expect(signup(email, password)).resolves.toBeUndefined();
-  expect(login(email, password)).resolves.toBeUndefined();
-  expect(logout()).resolves.toBeUndefined();
 });
