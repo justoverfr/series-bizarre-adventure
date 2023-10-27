d0050853a2d2b0f0b786869cec0cacabbb56fbc1
justoverfr <jeanjuste.lefebvre@gmail.com>
2023-10-27 09:59:06 +0200

    Connect login page to login function

--- src/components/BigButton.tsx
+++ src/components/BigButton.tsx
@@ -1,9 +1,13 @@
 interface BigButtonProps {
   text: string;
+  onClick: () => void;
 }
-export default function BigButton({ text }: BigButtonProps) {
+export default function BigButton({ text, onClick }: BigButtonProps) {
   return (
-    <button className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-transparent bg-[#6643b5] ">
+    <button
+      className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-transparent bg-[#6643b5]"
+      onClick={onClick}
+    >
       {text}
     </button>
   );
